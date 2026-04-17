930637f74b46db1aa06d202d4daca526e4654ffb
Guahh <t66305218@gmail.com>
2026-04-18 00:51:24 +1000

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,64 @@
+{
+  "totalCategories": 2,
+  "categories": [
+    {
+      "name": "IR",
+      "slug": "ir",
+      "count": 2,
+      "apps": [
+        {
+          "n": "Epson EB-695Wi IR Codes",
+          "d": "Epson projector IR file",
+          "s": "guahhinc/firetestground/Epson_EB-695Wi.ir",
+          "v": "1.0",
+          "slug": "epson-eb-695wi-ir",
+          "owner": "guahhinc",
+          "repo": "firetestground",
+          "commit": "main",
+          "path": "/",
+          "files": [
+            "Epson_EB-695Wi.ir"
+          ],
+          "category": "IR"
+        },
+        {
+          "n": "Sony Bravia IR Codes",
+          "d": "Sony Bravia TV IR Codes",
+          "s": "guahhinc/firetestground/Sony_Bravia.ir",
+          "v": "1.0",
+          "slug": "sony-bravia-ir",
+          "owner": "guahhinc",
+          "repo": "firetestground",
+          "commit": "main",
+          "path": "/",
+          "files": [
+            "Sony_Bravia.ir"
+          ],
+          "category": "IR"
+        }
+      ]
+    },
+    {
+      "name": "Tools",
+      "slug": "tools",
+      "count": 1,
+      "apps": [
+        {
+          "n": "Calculator",
+          "d": "M5StickC PLUS2 Calculator",
+          "s": "guahhinc/firetestground/M5Calculator.js",
+          "v": "1.1",
+          "slug": "calculator",
+          "owner": "guahhinc",
+          "repo": "firetestground",
+          "commit": "main",
+          "path": "/",
+          "files": [
+            "M5Calculator.js"
+          ],
+          "category": "Tools"
+        }
+      ]
+    }
+  ]
+}
