dffc1a9ca77c78e9cf84844b5efd3402ea9c24eb
Guahh <t66305218@gmail.com>
2026-04-18 11:19:17 +1000

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -84,7 +84,7 @@
             "n": "GuahhStore",
             "d": "Guahh Store App",
             "s": "guahhinc/firetestground/GuahhStore.js",
-            "v": "1.0.4",
+            "v": "1.0.3",
             "slug": "guahh-store",
             "owner": "guahhinc",
             "repo": "firetestground",
